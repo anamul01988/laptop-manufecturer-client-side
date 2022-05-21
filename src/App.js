65b2d2d5d7c6ae9803533ab8f2907d8070bfe906
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-05-22 01:32:22 +0600

    navbar and banner section added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Blog from "./Pages/Blog/Blog";
+import Dashboard from "./Pages/Dashboard/Dashboard";
+import Home from "./Pages/Home/Home";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Purchase from "./Pages/Purchase/Purchase";
+import Footer from "./Shared/Footer";
+import Navbar from "./Shared/Navbar";
 
 function App() {
   return (
     <div >
-         <h3>app</h3>
+      <Navbar></Navbar>
+      <Routes>
+         <Route path="/" element={<Home/>}></Route>
+         <Route path="/purchase" element={<Purchase/>}></Route>
+         <Route path="/dashboard" element={<Dashboard/>}></Route>
+         <Route path="/blogs" element={<Blog></Blog>}></Route>
+         <Route path="/portfolio" element={<Portfolio></Portfolio>}></Route>
+         {/* <Route path="" element={}></Route>
+         <Route path="" element={}></Route> */}
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import img1 from '../../assets/banner.jpg'
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row-reverse">
+          <img src={img1} class="max-w-sm md:max-w-sm lg:max-w-md  rounded-lg shadow-2xl" alt='img' />
+          <div>
+            <h1 class="text-5xl font-bold">Power Welshion Electronics Co. LTD</h1>
+            <p class="py-6">We have arrived with electronic computer parts for modern generation. Come and look for our services.
+            We are here to deliver our product to you. All products are auhentic and modern. Servcie quality will ensured must. So don't delay come and get our services.</p>
+            <button class="btn btn-primary">Read More</button>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Banner;
